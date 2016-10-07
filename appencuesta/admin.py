967b5eebe1ba510ca8b301680db3b12872d45ccd
Marcelo Alejandro Fernández <mar.ale.fer@gmail.com>
2016-10-07 07:54:01 -0300

    models al admin

--- appencuesta/admin.py
+++ appencuesta/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 
 from .models import Campania
-admin.site.register(Campania)
+from .models import Encuestador
+from .models import Parada
+from .models import Linea
+from .models import Lugar
+from .models import Motivo
+from .models import Encuesta
+
+admin.site.register(Campania, Encuestador, Parada, Linea, Lugar, Motivo, Encuesta)
