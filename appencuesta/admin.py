e065444fca9d656bf9c35fb235b685c7a55444b5
Marcelo Alejandro Fernández <mar.ale.fer@gmail.com>
2016-10-07 07:56:24 -0300

    ajuste admin

--- appencuesta/admin.py
+++ appencuesta/admin.py
@@ -8,4 +8,10 @@ from .models import Lugar
 from .models import Motivo
 from .models import Encuesta
 
-admin.site.register(Campania, Encuestador, Parada, Linea, Lugar, Motivo, Encuesta)
+admin.site.register(Campania)
+admin.site.register(Encuestador)
+admin.site.register(Parada)
+admin.site.register(Linea)
+admin.site.register(Lugar)
+admin.site.register(Motivo)
+admin.site.register(Encuesta)
